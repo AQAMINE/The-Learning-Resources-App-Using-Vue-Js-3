6acac0dc0801928a3499f4fec0b9065a29b48403
amine aqebli <aqebli36@gmail.com>
2022-04-08 03:48:59 +0000

    Add TheHeader-LearningResource-StoredResource components & Layout & Style

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <template>
-    <ul>
-        <learning-resources 
-        v-for="res in storedResources" 
-        :key="res.id"
-        :title="res.title"
-        :description="res.description"
-        :link="res.link"
-        ></learning-resources>
-    </ul>
-
+    <the-header title="RememberMe"></the-header>
+    <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import LearningResources from './components/learning-resources/LearningResource.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 export default {
     components:{
-        LearningResources
+        StoredResources,TheHeader
         },
     data() {
         return {
@@ -37,3 +30,19 @@ export default {
     },
 };
 </script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
